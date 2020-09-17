de19bca1277338f473cc07b7a19bc13bc801fe72
Ryan Houben <441943@student.fontys.nl>
2020-09-17 18:50:14 +0200

    - Added the RaceDiscipline class. - Added the RaceDisciplineContainer and it's methodes.

---
+++ RaceWorkshop.Logic/Classes/RaceDiscipline.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RaceWorkshop.Logic.Classes
+{
+    public class RaceDiscipline
+    {
+        public string Name { get; private set; }
+        public int Since { get; private set; }
+        public bool Active { get; private set; } = false;
+
+        public RaceDiscipline(string name, int since)
+        {
+            Name = name;
+            Since = since;
+        }
+    }
