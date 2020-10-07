a815c03f24b38e9ecf04fa3654bec679523610b8
Ryan Houben <441943@student.fontys.nl>
2020-10-07 10:06:33 +0200

    - Added some tests

---
+++ RaceWorkshop.UnitTest/RaceDisciplineTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RaceWorkshop.Logic.Classes;
+
+namespace RaceWorkshop.UnitTest
+{
+    [TestClass]
+    public class RaceDisciplineTest
+    {
+        [TestMethod]
+        public void RaceDiscipline_CreateSeason()
+        {
+            // Arrange
+            int numberOfRaces = 20;
+            int year = 2020;
+
+            RaceDiscipline raceDiscipline = new RaceDiscipline("Formula 1", 1980);
+
+            // Act
+            Season season = raceDiscipline.CreateSeason(numberOfRaces, year);
+
+            // Assert
+            season.NumberOfRaces.Should().Be(numberOfRaces);
+            season.Year.Should().Be(year);
+        }
+    }
+}
